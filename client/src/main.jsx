a6ebe210ce2075be2cb69919e0e8e339b237d19a
Artem Molchanov <artemmolchanov205@gmail.com>
2026-01-17 03:04:00 +1000

    .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,13 @@
 import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
 import "./index.css"
 import App from "./App.jsx"
-import { AuthProvider } from "../context/AuthContext.jsx"
-import { ChatProvider } from "../context/ChatContext.jsx"
+import { AuthContextProvider } from "./context/AuthContext.jsx"
+import { ChatProvider } from "./context/ChatContext.jsx"
 
 createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
-		<AuthProvider>
-			<ChatProvider>
-				<App />
-			</ChatProvider>
-		</AuthProvider>
-	</BrowserRouter>
+	<AuthContextProvider>
+		<ChatProvider>
+			<App />
+		</ChatProvider>
+	</AuthContextProvider>
 )
